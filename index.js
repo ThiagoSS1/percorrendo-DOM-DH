68c3ae9c3a5a9c43907f0c4cf46ee37cd4a96f92
Thiago Seta <thiago_seta@hotmail.com>
2023-02-27 20:43:47 -0300

    subindo repo

---
+++ index.js
@@ -0,0 +1,21 @@
+// body
+document.querySelector('body')
+
+    // div 
+    document.querySelector('.conteudo')
+        // ul
+        document.querySelector('.conteudo ul')
+            // li
+            document.querySelector('.conteudo ul li')
+
+    // h1
+    document.querySelector('h1')
+        // conteudo h1
+         document.querySelector('h1').textContent
+
+    // section
+    document.querySelector('section')
+        // article
+        document.querySelector('section article')
+            // p
+            document.querySelector('section article p')
